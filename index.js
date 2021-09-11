3004831b318020e1984b7a1d59bc0c400be0e1a4
Jesmin Juthi <45585097+jesmin-juthi@users.noreply.github.com>
2021-09-11 15:56:42 +0600

    js array

---
+++ index.js
@@ -0,0 +1,84 @@
+// var array1 = ["jesmin", "juthi", "sathil", "korobi", "israt"];
+
+// array er full elements show korbe
+// console.log(array1); 
+
+// 0 index e jei element ache oita show korbe
+// console.log(array1[0]);
+
+// array er index onujai elements er man poriborton
+
+// array1[3] = "emu"; 
+
+// console.log(array1);
+
+// push method - array er last e new elements add kora 
+
+// array1.push("emu");
+
+// console.log(array1);
+
+// pop method - array er ekdom sesh element remove kora 
+
+// array1.pop();
+
+// console.log(array1);
+
+// shift method - array er 1st element remove kora
+
+// array1.shift();
+
+// console.log(array1);
+
+// unshift method - array er 1st dike elements add kora 
+
+// array1.unshift("jayed", "rohan");
+
+// console.log(array1);
+
+
+// remove last element from array && transfer to another array - method 1
+
+
+// var fruits = ["banana", "mango", "apple", "orange", "tiger"];
+
+// var animals = ["lion", "monkey", "donkey", "cat", "dog"];
+
+
+// var removeElement = fruits.pop();
+
+// animals.push();
+
+// console.log(fruits, animals);
+
+
+// remove element middle of the array && transfer to another array - Method 2
+
+// var fruits = ["banana", "mango", "tiger", "apple", "orange"];
+
+// var animals = ["lion", "monkey", "donkey", "cat", "dog"];
+
+// var removeElement = fruits[2];
+
+// animals.push(removeElement);
+
+// delete(fruits[2]); 
+// eivabe delete korle o oitai ekta empty man hoye jay.. 
+
+// console.log(animals, fruits); 
+
+
+// remove element middle of the array && transfer to another array - Method 3 [workable]
+
+// var fruits = ["banana", "mango", "tiger", "apple", "orange"];
+
+// var animals = ["lion", "monkey", "donkey", "cat", "dog"];
+
+// var removeElement = fruits.splice(2, 1);
+
+// animals.push(removeElement);
+
+// console.log(animals.length, fruits.length);
+
+
+
